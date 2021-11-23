03a317985fe1b41e5811deee02460a8d5d170f69
ezekielr98 <Ezequielroldan9887@hotmail.com>
2021-11-23 12:41:44 -0300

    actualizacion2.0

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
- const express = require('express')
-const { dirname } = require('path/posix')
-const app = express()
-app.use(express.static('public'))
-const port = 3000
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
 
 app.listen (process.env.PORT ||3000, ()=>{
     console.log('Servidor funcionando bien');
 });
-app.get('/', (req, res) => {res.sendFile(__dirname + ('/views/Home.html'))})
 
-app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/views/register.html')
-    });
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html')
-    });
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+/*process.env.PORT para correrlo en el server" administra heroku un puerto"*/
