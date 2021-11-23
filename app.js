c281b0b1721da83ce789cda22c70c010d37a2e1b
ezekielr98 <Ezequielroldan9887@hotmail.com>
2021-11-23 11:46:46 -0300

    actualizacion

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express()
 app.use(express.static('public'))
 const port = 3000
 
-
-app.listen(port, () => console.log(`server function`))
+app.listen (process.env.PORT ||3000, ()=>{
+    console.log('Servidor funcionando bien');
+});
 app.get('/', (req, res) => {res.sendFile(__dirname + ('/views/Home.html'))})
 
 app.get('/register', (req, res) => {
